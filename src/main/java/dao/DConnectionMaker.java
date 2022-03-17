8b50e0cf4c0c2e69de393bf45bca98bb7195628b
Your Name <you@your-domain.com>
2022-03-17 19:33:55 +0900

    2022.03.17
    토비의 스프링 학습

--- src/main/java/dao/DConnectionMaker.java
+++ src/main/java/dao/DConnectionMaker.java
@@ -9,6 +9,7 @@ public class DConnectionMaker implements ConnectionMaker{
 	@Override
 	public Connection makeConnection() throws ClassNotFoundException, SQLException {
 		Class.forName("com.mysql.jdbc.Driver");
+		// useUnicode=true&characterEncoding=utf8 를 써주어야 한다.
 		Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/springbook?useUnicode=true&characterEncoding=utf8", "root", "root");
 		return c;
 	}
